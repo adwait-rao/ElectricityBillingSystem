ee9d80fe1cc46913a26599171a0587430b219d58
adwaitrao7000 <71839093+adwaitrao7000@users.noreply.github.com>
2022-05-20 20:46:56 -0700

    Added Functional Login Page
    pls make sure you change the connection string datasource path, replace the username adwait with your pc username to make the program work properly, i will find the way to use the relative path soon:-)

--- ElectricityBillingSystem/ElectricityBillingSystem/Form1.vb
+++ ElectricityBillingSystem/ElectricityBillingSystem/Form1.vb
@@ -1,10 +1,3 @@
 ﻿Public Class Form1
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
-    End Sub
 End Class
---
+++ ElectricityBillingSystem/ElectricityBillingSystem/login.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Data.OleDb
+
+Public Class login
+    Dim read As String
+    Dim datafile As String
+    Dim connstring As String
+    Dim myconnection As OleDbConnection = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\adwaitrao\Documents\GitHub\ElectricityBillingSystem\ElectricityBillingSystem\ElectricityBillingSystem\ElectricityBillingDB.accdb")
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '  read = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\adwaitrao\Documents\GitHub\ElectricityBillingSystem\ElectricityBillingSystem\ElectricityBillingSystem\ElectricityBillingDB.accdb"
+        '  datafile = "C:\Users\adwaitrao\Documents\GitHub\ElectricityBillingSystem\ElectricityBillingSystem\ElectricityBillingSystem\ElectricityBillingDB.accdb"
+        'connstring = read & datafile
+
+        'myconnection.ConnectionString = connstring
+
+        Dim cmd As OleDbCommand = New OleDbCommand("select *from AdminUsers Where adminName = '" & TextBox1.Text & "' and passwd = '" & TextBox2.Text & "'", myconnection)
+        myconnection.Open()
+
+        Dim dr As OleDbDataReader = cmd.ExecuteReader
+        Dim userFound As Boolean = False
+        Dim adminUsername As String = ""
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+
+            If TextBox1.Text = "" Then
+                ErrorProvider1.SetError(TextBox1, "Enter Your Username")
+            End If
+            If TextBox2.Text = "" Then
+                ErrorProvider1.SetError(TextBox2, "Enter Your Password")
+            End If
+        Else
+            Try
+                While dr.Read
+                    userFound = True
+                    adminUsername = dr("adminName").ToString
+                End While
+
+                If userFound = True Then
+                    Form1.Show()
+                Else
+                    MsgBox("Sorry, No User Found!", MsgBoxStyle.Exclamation)
+                End If
+            Catch ex As Exception
+                MsgBox(ex, MsgBoxStyle.Exclamation)
+            Finally
+                myconnection.Close()
+            End Try
+        End If
+    End Sub
+End Class
